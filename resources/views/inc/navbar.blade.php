46a1348647a319f58ae0d5dccfd17b12d3925fd9
Oksana Repkina <repkinaoxana@gmail.com>
2017-09-24 12:31:04 +0300

    Project init

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar main-menu navbar-default">
+    <div class="container">
+        <div class="menu-content">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">e.less</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav text-uppercase">
+                    <li><a data-toggle="dropdown" class="dropdown-toggle" href="/">Home</a>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>e.less</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('inc.navbar')
+@yield('content')
+
+<script src="/js/app.js"></script>
+</body>
+</html>
